7128e8d3bfe04ce96c372a5865bcc1f66ccc9b71
KatePril <ket14pril14@gmail.com>
2023-06-26 18:14:00 +0300

    Change admin

--- ice_cream/admin.py
+++ ice_cream/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import IceCream
 
-admin.site.register(IceCream)
+# admin.site.register(IceCream)
+@admin.register(IceCream)
+class IceCreamAdmin(admin.ModelAdmin):
+    pass
