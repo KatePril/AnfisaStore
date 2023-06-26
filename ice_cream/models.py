8f65bd0b799076d87c6c73917494f4fe2d0566f5
KatePril <ket14pril14@gmail.com>
2023-06-26 17:39:31 +0300

    add price, quantity to IceCream model

--- ice_cream/models.py
+++ ice_cream/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class IceCream(models.Model):
     name = models.CharField('Название', max_length=200)
     description = models.TextField('Описание')
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     on_main = models.BooleanField('На главную?', default=True)
     image = models.ImageField(upload_to='ice_cream/', verbose_name='Зображення', blank=True, null=True)
 
