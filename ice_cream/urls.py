d79ff1d464d542d892387f3981a67baa34a5d714
KatePril <ket14pril14@gmail.com>
2023-07-02 11:14:49 +0300

    Fix main_image

--- ice_cream/urls.py
+++ ice_cream/urls.py
@@ -7,15 +7,10 @@ app_name = 'ice_cream'
 
 urlpatterns = [
     # Главная страница
-    
-    # path('', index, name='index'),
     path('', IceCreamIndex.as_view(), name='index'),
-    
     # Список мороженого
     path('ice_cream/', IceCreamList.as_view(), name='ice_cream_list'),
-    # path('ice_cream/', ice_cream_list, name='ice_cream_list'),
     # Подробная информация о мороженом. Ждем пременную типа int, 
-    # и будем использовать ее под именем pk IceCreamDetail.as_view()
-    # path('ice_cream/<int:pk>/', ice_cream_detail, name='ice_cream_detail'),
+    # и будем использовать ее под именем pk
     path('ice_cream/<int:pk>/', IceCreamDetail.as_view(), name='ice_cream_detail'),
 ]
--- ice_cream/views.py
+++ ice_cream/views.py
@@ -1,52 +1,18 @@
+from typing import Any
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import IceCream
 
-def index(request):
-    template = 'ice_cream/index.html'
-    selected_ice_creams = IceCream.objects.filter(on_main='True')
-    # только то мороженое, у кторого есть флаг on_main
-    context = {
-        'selected_ice_creams': selected_ice_creams,
-    }
-    return render(request, template, context)
-
-
-# def ice_cream_list(request):
-#     template = 'ice_cream/ice_cream_list.html'
-#     ice_creams = IceCream.objects.all()
-#     # все мороженое
-#     context = {
-#         'ice_creams': ice_creams,
-#     }
-#     return render(request, template, context)
-
-
-# def ice_cream_detail(request, pk):
-#     template = 'ice_cream/ice_cream_detail.html'
-#     ice_cream = get_object_or_404(IceCream, id=pk)
-#     # Мороженое с id из запроса
-#     # print(ice_cream)
-#     print()
-#     print(ice_cream.images())
-#     print()
-#     context = {
-#         'ice_cream': ice_cream,
-#         'images': ice_cream.images,
-#     }
-#     # for image in ice_cream.images:
-#     #     if image.is_main == True:
-#     #         context['main_image'] = image
-#     return render(request, template, context)
 
 class IceCreamIndex(ListView):
     model = IceCream
-    template_name = ('icecream_index.html')
+    queryset = IceCream.objects.filter(on_main='True')
+    template_name = 'ice_cream/icecream_index.html'
 
 class IceCreamList(ListView):
     model = IceCream
-    template_name = ('icecream_list.html')
+    template_name = 'templates/ice_cream/icecream_list.html'
     
 class IceCreamDetail(DetailView):
     model = IceCream
