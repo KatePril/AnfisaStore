182dc63d094d20f52a3a88c1b454dd320ce7a5ee
KatePril <ket14pril14@gmail.com>
2023-07-12 18:02:57 +0300

    Add forms, urls, models

--- order/models.py
+++ order/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+from ice_cream.models import IceCream
 # Create your models here.
+
+class Cart(models.Model):
+    ice_cream = models.ForeignKey(IceCream, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.ice_cream.name} - {self.quantity}'
--- order/views.py
+++ order/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import AddCartForm
+from .models import Cart
 
+from django.http import HttpResponse
 # Create your views here.
+def get_cart_data(user_id):
+    cart = Cart.objects.filter(user=user_id)
+    total = 0
+    for row in cart:
+        total += row.ice_cream.price * row.quantity
+    return {'cart': cart, 'total': total}
+
+def add_to_cart(request):
+    data = request.GET.copy()
+    data.update({'user': request.user.id})
